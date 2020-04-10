cd5cee3bfc78740a1b4a308de3d9f88cae44e77b
meyer337 <meyer337@purdue.edu>
2020-04-10 01:29:17 -0400

    Fixed mostly

--- tips.java
+++ tips.java
@@ -1,8 +1,21 @@
+import java.io.*;
+/**
+ * tips.java
+ *
+ * This program is for automatically calculating tips for those who
+ *  worked for a specific day.
+ *
+ * @author Brett Meyer
+ */
+
 import java.util.Scanner;
 
 public class tips {
   public static void main(String [] args) {
-    Scanner scanner = new Scanner(System.in); 
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("~~TippyBoy 3000 Booting Up~~\n");
 
     System.out.print("Enter the date: ");
     String date = scanner.nextLine();
@@ -23,7 +36,8 @@ public class tips {
     System.out.println("Worker info: ");
 
     boolean done = true;
-    for (int i = 0; i < numPeople; i++) { 
+    for (int i = 0; i < numPeople; i++) {
+
       scanner.nextLine();
       System.out.print("Name " + (i+1) +": ");
       names[i] = scanner.nextLine();
@@ -32,33 +46,33 @@ public class tips {
       hours[i] = scanner.nextDouble();
     }
 
+    //Count number of total hours
     double totalHours = 0;
-    for (int j = 0; j < numPeople; j++) {
-      totalHours += hours[j];
+    for (int i = 0; i < numPeople; i++) {
+
+      totalHours += hours[i];
+
     }
 
     double totalClaimed = creditTips / totalHours;
     double totalTips = (creditTips + cashTips) / totalHours;
-    String[] indClaim = new String[numPeople];
-    String[] indTotal = new String[numPeople];
-
-    for (int k = 0; k < numPeople; k++) {
-      String formattedClaim = String.format("%.2f", totalClaimed * hours[k]);
-      indClaim[k] = formattedClaim;
-      String formattedTotal = String.format("%.2f", totalTips * hours[k]);
-      indTotal[k] = formattedTotal;
-    }
 
     System.out.println("");
     System.out.println("==============");
     System.out.println("");
+
     System.out.printf("Date: %s\n", date);
     System.out.println("Total tips: $" + (creditTips + cashTips));
-    System.out.printf("Total hours worked: %.2f\n", totalHours);
-    for (int l = 0; l < numPeople; l++) {
-      System.out.printf(names[l] + ":\n" + "Claim: " + indClaim[l] + "\n" + "Total: " + indTotal[l]);
-      System.out.println("");
-      System.out.println("");
+    System.out.printf("Total hours worked: %.2f\n\n", totalHours);
+
+    for (int i = 0; i < numPeople; i++) {
+
+      double claim = totalClaimed * hours[i];
+      double total = totalTips * hours[i];
+
+      System.out.printf("%s:\nClaim: %.2f\nTotal: %.2f\n\n", names[i], claim, total);
     }
+
+    System.out.println("All done :) DO NOT PRESS ANYTHING UNLESS YOU WANT TO DO THIS OVER\n");
   }
 }
